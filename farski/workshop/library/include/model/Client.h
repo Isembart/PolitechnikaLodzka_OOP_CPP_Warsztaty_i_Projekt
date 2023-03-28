b7c6c38e80991fa3f909f7bd2419c39fe8df30ce
247653 Farski Maurycy <247653@edu.p.lodz.pl>
2023-03-29 01:00:58 +0200

    Etap 2 i Etap 3 done!

--- farski/workshop/library/include/model/Client.h
+++ farski/workshop/library/include/model/Client.h
@@ -6,13 +6,14 @@ class Client {
 private:
     std::string firstName;
     std::string lastName;
-    int personalID;
+    const int personalID;
+    Client();
 public:
     void setFirstName(std::string);
     void setLastName(std::string);
     void setPersonalID(int);
     std::string getClientInfo();
-    Client();
+    Client(std::string,std::string ,int);
     ~Client();
 };
 
--- farski/workshop/library/src/model/Client.cpp
+++ farski/workshop/library/src/model/Client.cpp
@@ -7,29 +7,45 @@
 
 void Client::setFirstName(std::string fName)
 {
-    firstName = fName;
+    if(fName.length() > 0)
+    {
+        firstName = fName;
+    }
 }
 
 void Client::setLastName(std::string lName)
 {
-    lastName = lName;
+    if(lName.length() > 0)
+    {
+        lastName = lName;
+    }
 }
 
-void Client::setPersonalID(int id)
-{
-    personalID = id;
-}
+//void Client::setPersonalID(int id)
+//{
+    //personalID = id;
+//}
 
 std::string Client::getClientInfo()
 {   
     return firstName + lastName + std::to_string(personalID);
 }
 
-Client::Client()
-{
-    std::cout<<"Utworzono instancje klasy client! adres: "<< this<<std::endl;
+//Nie używamy konstruktora domyślnego
+//Client::Client()
+//{
+    //firstName = "Test";
+    //lastName = "Test";
+    //personalID = 0;
+    //std::cout<<this<<": Uzylem konstruktora domyslnego!"<<std::endl;
+    //std::cout<<getClientInfo()<<std::endl;
+//}
+
+Client::Client(std::string fName, std::string lName, int id) : firstName(fName) , lastName(lName) , personalID(id) {
+    //std::cout<<this<<": Uzylem konstruktora parametrowego!"<<std::endl;
+    //std::cout<<getClientInfo()<<std::endl;
 }
 
 Client::~Client(){
-    std::cout<<"Zniszczono instancje klasy client! adres: "<<this<<std::endl;
+    //std::cout<<"Zniszczono instancje klasy client! adres: "<<this<<std::endl;
 }
