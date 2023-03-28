fd05f2f48b5181ecd0b8d43c37489ab050ea5fbc
247653 Farski Maurycy <247653@edu.p.lodz.pl>
2023-03-28 21:25:22 +0200

    initial commit

---
+++ farski/introduction/program/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "math_helpers.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "hello world!"<<endl;  
+    cout << factorial(5) << endl;
+    return 0;
+}
