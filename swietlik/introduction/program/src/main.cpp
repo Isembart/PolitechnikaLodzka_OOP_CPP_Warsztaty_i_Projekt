49551885ea7b4002312a7bca8641ed6b3f66d91b
Agata Swietlik <247809@edu.p.lodz.pl>
2023-03-29 13:25:33 +0200

    init commit

---
+++ swietlik/introduction/program/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "math_helpers.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello, hello./p" << endl;
+    cout << factorial(5) << endl;
+
+
+    return 0;
+}
