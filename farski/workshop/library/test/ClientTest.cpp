2e3ef4793176ab442beeba7bfa484bc61e2f6f8e
247653 Farski Maurycy <247653@edu.p.lodz.pl>
2023-04-10 16:29:47 +0200

    Revert "Na zapas aby pokazać na zajęciach"
    This reverts commit 5ff12948455f32a0438458878bfc96fe64aec291.

--- farski/workshop/library/test/ClientTest.cpp
+++
@@ -1,9 +0,0 @@
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(TestSuiteClient)
-
-    BOOST_AUTO_TEST_CASE(AssertionsTests) {
-        BOOST_TEST(true);
-    }
-
-BOOST_AUTO_TEST_SUITE_END()
