0563845e4a140b945fc91714473a86c390ea707a
247653 <247653@edu.p.lodz.pl>
2023-04-16 16:28:05 +0200

    testy Clienta

--- farski/workshop/library/test/ClientTest.cpp
+++ farski/workshop/library/test/ClientTest.cpp
@@ -8,11 +8,27 @@ BOOST_AUTO_TEST_SUITE(TestSuiteClient)
         BOOST_TEST(true);
     }
 
-    BOOST_AUTO_TEST_CASE(ConstructorTest) {
-        Client* client1 = new Client("Mariusz","Pudzianowski",151413);
-        BOOST_TEST(client1->getFirstName() == "Mariusz");
-        BOOST_TEST(client1->getLastName() == "Pudzianowski");
-        BOOST_TEST(client1->getPersonalID() == 151413);
+    Client* client1 = new Client("Mariusz","Pudzianowski",151413);
+    BOOST_AUTO_TEST_CASE(GettersTests) {
+        //Testy getterów | _REQUIRE daltego że jeśli gettery nie działają to testy setterów korzystające z nich i tak nie będą wiarygodne
+        BOOST_TEST_REQUIRE(client1->getFirstName() == "Mariusz");
+        BOOST_TEST_REQUIRE(client1->getLastName() == "Pudzianowski");
+        BOOST_TEST_REQUIRE(client1->getPersonalID() == 151413);
     }
+
+    BOOST_AUTO_TEST_CASE(SettersTests) {
+        //Testy setFirstName
+        client1->setFirstName("Jonasz");
+        BOOST_TEST(client1->getFirstName()=="Jonasz");
+        client1->setFirstName("");
+        BOOST_TEST(client1->getFirstName()=="Jonasz");
+
+        //Testy setLastName
+        client1->setLastName("josephson");
+        BOOST_TEST(client1->getLastName()=="josephson");
+        client1->setLastName("");
+        BOOST_TEST(client1->getLastName()=="josephson");
+    }
+
 
 BOOST_AUTO_TEST_SUITE_END()
