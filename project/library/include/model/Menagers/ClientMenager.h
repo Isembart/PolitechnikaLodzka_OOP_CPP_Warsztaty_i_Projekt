8622111cea9ef326487d1215a39ae5325b2d38e7
Agata Swietlik <247809@edu.p.lodz.pl>
2023-06-19 09:41:06 +0200

    clientmenager add

---
+++ project/library/include/model/Menagers/ClientMenager.h
@@ -0,0 +1,14 @@
+//
+// Created by student on 19.06.23.
+//
+
+#ifndef VIRTALMACHINERENTING_CLIENTMENAGER_H
+#define VIRTALMACHINERENTING_CLIENTMENAGER_H
+
+
+class ClientMenager {
+
+};
+
+
+#endif //VIRTALMACHINERENTING_CLIENTMENAGER_H
