175d67acb3f0af100597010342ec46a70362cffe
247653 <247653@edu.p.lodz.pl>
2023-06-21 23:17:43 +0200

    Basic Manager Tests

--- project/library/test/ClientManagerTest.cpp
+++ project/library/test/ClientManagerTest.cpp
@@ -1,9 +1,9 @@
 #include <boost/test/unit_test.hpp>
-#include "model/Managers/ClientManager.h"
-#include "repositories/ClientRepository.h"
-#include "typyedefs.h"
+#include "model/Managers/ClientManager.hpp"
+#include "model/Repositories/ClientRepo.hpp"
+#include "typedefs.hpp"
 #include <memory>
-#include "model/Client.h"
+#include "model/Client.hpp"
 
 struct ClientManagerFixture {
 
@@ -19,28 +19,28 @@ struct ClientManagerFixture {
 BOOST_FIXTURE_TEST_SUITE(ClientManagerTests,ClientManagerFixture)
 
 BOOST_AUTO_TEST_CASE(regsiterClient) {
-    manager.registerClient("Mariusz","Pudzianowski",14,nullptr,nullptr);
+    manager.registerClient("Mariusz","Pudzianowski",14,nullptr);
 
-    ClientPtr ptr = manager.registerClient("Mariusz","Pudzianowski",13,nullptr,nullptr);
+    ClientPtr ptr = manager.registerClient("Mariusz","Pudzianowski",13,nullptr);
     
-    manager.registerClient("Mariusz","Pudzianowski",15,nullptr,nullptr);
+    manager.registerClient("Mariusz","Pudzianowski",15,nullptr);
 
-    BOOST_TEST(manager.registerClient("Drugi","Pudzian LOL",13,nullptr,nullptr)==ptr);
+    BOOST_TEST(manager.registerClient("Drugi","Pudzian LOL",13,nullptr)==ptr);
 }
 
 BOOST_AUTO_TEST_CASE(getClient) {
 
-    manager.registerClient("Mariusz","Pudzianowski",14,nullptr,nullptr);
-    ClientPtr ptr = manager.registerClient("Mariusz","Pudzianowski",13,nullptr,nullptr);
-    manager.registerClient("Mariusz","Pudzianowski",15,nullptr,nullptr);
+    manager.registerClient("Mariusz","Pudzianowski",14,nullptr);
+    ClientPtr ptr = manager.registerClient("Mariusz","Pudzianowski",13,nullptr);
+    manager.registerClient("Mariusz","Pudzianowski",15,nullptr);
 
     BOOST_TEST(ptr == manager.getClient(13));
 }
 
 BOOST_AUTO_TEST_CASE(findClients) {
-    manager.registerClient("Pierwszy","test",14,nullptr,nullptr);
-    manager.registerClient("Drugi","1",15,nullptr,nullptr);
-    manager.registerClient("Trzeci","Pudzianowski",16,nullptr,nullptr);
+    manager.registerClient("Pierwszy","test",14,nullptr);
+    manager.registerClient("Drugi","1",15,nullptr);
+    manager.registerClient("Trzeci","Pudzianowski",16,nullptr);
 
     ClientPredicate isPudzianowski = [](ClientPtr ptr) {
         return ptr->getLastName() == "Pudzianowski";
