40da6dea6afc1f72c872ef36754649b7c2d52c00
Agata Swietlik <247809@edu.p.lodz.pl>
2023-06-22 10:06:32 +0200

    RentRepo test

--- project/library/test/RentRepositoryTest.cpp
+++ project/library/test/RentRepositoryTest.cpp
@@ -1,6 +1,9 @@
 #include <boost/test/unit_test.hpp>
 #include "model/Repositories/RentRepo.hpp"
 #include "model/Repositories/StorageContainer.h"
+#include "model/Rent.hpp"
+#include <boost/test/unit_test.hpp>
+#include <memory>
 
 struct TestSuiteRentRepoFixture {
     StorageContainer container;
@@ -15,4 +18,31 @@ struct TestSuiteRentRepoFixture {
 BOOST_FIXTURE_TEST_SUITE(TestSuiteRentRepo,TestSuiteRentRepoFixture)
 
 
-BOOST_AUTO_TEST_SUITE_END()
+    BOOST_FIXTURE_TEST_SUITE(TestSuiteRenttRepo,TestSuiteRentRepoFixture)
+
+
+        BOOST_AUTO_TEST_CASE(getValidIndexTest) {
+            BOOST_TEST(container.getRentRepo()->get(0)->getId() == 666);
+            BOOST_TEST(container.getRentRepo()->get(1)->getId() == 333);
+        }
+
+        BOOST_AUTO_TEST_CASE(getInvalidIndextText) {
+            BOOST_TEST(container.getRentRepo()->get(-40) == nullptr);
+            BOOST_TEST(container.getRentRepo()->get(-52) == nullptr);
+        }
+
+
+        BOOST_AUTO_TEST_CASE(sizeTest) {
+            BOOST_TEST_REQUIRE(container.getRentRepo()->size() == 2);
+        }
+
+
+        BOOST_AUTO_TEST_CASE(reportTest) {
+            std::string info = container.getRentRepo() ->get(0)->getRentInfo() + "\n" + container.getRentRepo()->get(1)->getRentInfo() + "\n";
+            BOOST_TEST(container.getRentRepo()->report() == info);
+        }
+
+
+    BOOST_AUTO_TEST_SUITE_END()
+}
+
